bbc63c6d46e5a6a196e0223c92dd177eb1664a60
Giulia Cardieri <giulia.cardieri@gmail.com>
2019-12-14 16:29:46 -0300

    Update hero image

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,51 @@
 <template>
   <div id="giuliacardieri" class="container">
     <section class="section">
-      <figure class="section__svg">  
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 155.7 176.9">
-            <linearGradient id="pink" gradientUnits="userSpaceOnUse" x1="58.0506" y1="80.3482" x2="58.0506" y2="36.8212" gradientTransform="matrix(1 0 0 -1 0 178.0107)">
-              <stop offset="0" stop-color="#332c28"/>
-              <stop offset=".1651" stop-color="#332c28"/>
-              <stop offset=".5064" stop-color="#912580"/>
-              <stop offset=".8061" stop-color="#8f2686"/>
-            </linearGradient>
-            <linearGradient id="brown" gradientUnits="userSpaceOnUse" x1="58.0506" y1="80.3482" x2="58.0506" y2="36.8212" gradientTransform="matrix(1 0 0 -1 0 178.0107)">
-              <stop offset="0" stop-color="#332c28"/>
-              <stop offset=".1651" stop-color="#332c28"/>
-              <stop offset=".5064" stop-color="#332c28"/>
-              <stop offset=".8061" stop-color="#332c28"/>
-            </linearGradient>
-            <linearGradient id="purple" gradientUnits="userSpaceOnUse" x1="58.0506" y1="80.3482" x2="58.0506" y2="36.8212" gradientTransform="matrix(1 0 0 -1 0 178.0107)">
-              <stop offset="0" stop-color="#332c28"/>
-              <stop offset=".1651" stop-color="#332c28"/>
-              <stop offset=".5064" stop-color="#45007f"/>
-              <stop offset=".8061" stop-color="#49006a"/>
-            </linearGradient>
-            <path class="hair" d="M41.5 128.1c-.5 3.3.1 17.8.6 18.4.7.7 13.1.9 15 .5 1.6-.3 15.6.3 16.7 1 1.5.8 2.8-28.6 2.2-30.9s-.3-24-.9-24.2c-3-1.5-34.5-2-34.8.3-1.6 11.8 1.7 31.7 1.2 34.9z" fill="url(#brown)"/>
-            <path class="hair" d="M85.6 124.1c-.4 3.3-.9 16.5-.5 17.1.5.7 9.9 2.4 11.2 2 1.1-.3 16.1-1.2 17-.5 1.1.8-2.3-27.3-2.7-29.6-.4-2.3 7.4-21.1 7-21.4-2.2-1.5-33.7-4.3-34-2-1.2 11.8 2.4 31.2 2 34.4z" fill="url(#brown)"/>
-            <path fill="#eee1d2" d="M63.6 105.8h29v41.3h-29z"/>
-            <path d="M92.6 124.9S82.5 122.3 79 122c-4.1-.4-15.4-2.7-15.4-2.7l.1-4.9 28.9-2v12.5z" opacity=".4" fill="#d5c9bd"/>
-            <path d="M122.7 60.6c0 16.1-3.7 30.8-14.1 40.8-9.1 8.7-18 15.7-31.2 15.7-13.4 0-21.4-4.7-30.5-13.7-10.1-10-16.5-22.5-16.5-38.3 0-10.2.2-18.2 1-29.3.6-9.1 7.4-14.1 16.5-19.9 8.5-5.4 20.4-6.3 29.7-6.3 12 0 26.9.8 35.1 8 5.7 5 9.8 9.2 9.8 16.5.1 10 .2 17.5.2 26.5z" fill="#eee1d2" stroke="#ecddcb" stroke-miterlimit="10"/>
-            <path class="st8" d="M36.1 82.3c-1.8-4.9-2.8-9.9-2.5-11.8 1.7-11.3.3-2.9 1.7-11.3.9-5.4 64.8-39.5 64.8-39.5L75.8.1s-48 1.1-57.9 24.3c-1.9 4.4-2.9 8.8-3.8 12.7-2.4 11.4-5.5 16.3-6 23.5-.6 8.5 4.3 16.2 5.1 24.8.2 1.7-3.3 11.5-1.1 11.6 4 .1 30.3 5.4 30.1.9-.2-5.3-6-15.4-6.1-15.6"/>
-            <path class="st8" d="M115.9 80.6c1.1-4.9 3.8-8.5 4-11.5.5-9-2-21.6-3-28.3-1.1-7.8-7.2-17.6-10-19-4.6-2.4-31.3-5.6-31.3-5.6V.1s49-2.3 57.6 21.5c7.9 21.8 7.6 25.6 8.2 34.8.6 8.5 2.7 7.8 2.3 19.6-.2 5.6-3.9 13.1-3.7 18.4.2 4.7-26.9 1-26.2.3"/>
-            <path class="hair" d="M11.8 113.2c.7 1.9 3.5 6.4 2.6 8.8-1.4 3.6-1.9 2.9-2.9 6.2-1 3.3.6 17.8 1.7 18.3 1.5.7 7.2-2.5 13-3.5 8.2-1.4 18 5.2 22 4.8 3.4-.3 2.8-11.5 3.9-14.1 1.2-2.8.1-6.6-1.1-8.9s-5.3-6.1-6.3-8.2c-2-4.2-1.3-5.2-2.4-17.8 0-.3-.9-4.9-1.7-6C37 87.3 12.7 89 12 91.3c-3.6 11.8-1.3 18.8-.2 21.9z" fill="url(#brown)"/>
-            <path class="hair" d="M103.4 120.1c-.5 2.5 1 5.1-.1 8.4-1 3.3.9 15.1 2.1 15.7.9.4 6.4-.8 8.8-.2 9 2.1 13.1 5.8 17.2 5.5 3.4-.3 3.3-4.8 5.5-6.8 2.4-2 .8-10.4-.3-12.6-1.2-2.5 5.2-12 4.8-16.1-.3-3.2-1.2-9.6-1.7-14.9 0-.3-.2-2-.1-4.2.3-6.6-25.3-2.8-26-.5-1.4 4.6-6 12.7-7.4 15.8-2.1 4.9-2.4 7.9-2.8 9.9z" fill="url(#brown)"/>
-            <path d="M120.2 176.8c-4 0-62.2-.2-72.8-.2-8.7 0-12.2-.6-10-6.5 4-11.1-5.3-28.7-.6-31.8 3.8-2.5 42.4-.6 48.1-.6 5.8 0 20.9.2 28.6 1.1 2 .2 3.9.1 4.6.2 7.1.6 6.1 37.8 2.1 37.8z" fill="#efe2d3"/>
-            <path class="st14" d="M15.3 176.8c-9.1.2-5.9 0-14.9 0-.6 0-.5 0 .5-1.9 1.2-2.4 3.5-7.4 4.8-11.2 4-11.1 7.7-23.9 13.2-24.9 3.2-.6 7.4-.9 12.3-1.2 5.1-.3 3.8 0 6.6 0 3.3 0 5.2 6.6 1.7 14.1-3.9 8.4-7 17.7-8.1 23.8-.1.4-.2 1.3-.5 1.3-3.5.2-7.7-.1-15.6 0z"/>
-            <path class="st14" d="M79.8 176.8c-20.4-.3-27.4 0-46.8 0-1.7 0-.2-4.5.1-6.2 1.3-8.4 4.8-33 4.8-33s26.7 14.5 39.8 14.5c15.5 0 40.1-13.2 40.1-13.2s5 22.5 6.4 30c.3 1.6 1.4 7.8-.3 7.8-10.9.1-24.4.4-44.1.1z"/>
-            <path class="st14" d="M140 176.8c9.1.1 6.3-.1 15.1 0 1.9 0-.8-5.3-3.1-11.6-4.1-11.1-9.2-23.4-14.7-24.5-2.2-.5-5.5-.5-10-1-4.5-.4-6.4-.6-8.7-.7-.5 0-1.5-.3-1.4.3.3 5.6-2.9 11.1 0 17.7 4.1 9.2 9.1 13.3 10.6 19.2.1.4.1.7.4.7 1.6-.1 5.1-.1 11.8-.1z"/>
-            <path fill="none" stroke="#4f4f4f" stroke-width=".25" stroke-miterlimit="10" d="M62.9 137.1l14.6 6.3 15.1-5.6"/>
-            <path fill="#4f4f4f" d="M78.6 142.7l-1.1 1.9-1.1-1.9z"/>
-          </svg>
-      </figure>
+      <section class="section__image">
+        <figure class="section__svg">  
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 155.7 176.9">
+              <linearGradient id="pink" gradientUnits="userSpaceOnUse" x1="58.0506" y1="80.3482" x2="58.0506" y2="36.8212" gradientTransform="matrix(1 0 0 -1 0 178.0107)">
+                <stop offset="0" stop-color="#332c28"/>
+                <stop offset=".1651" stop-color="#332c28"/>
+                <stop offset=".5064" stop-color="#912580"/>
+                <stop offset=".8061" stop-color="#8f2686"/>
+              </linearGradient>
+              <linearGradient id="brown" gradientUnits="userSpaceOnUse" x1="58.0506" y1="80.3482" x2="58.0506" y2="36.8212" gradientTransform="matrix(1 0 0 -1 0 178.0107)">
+                <stop offset="0" stop-color="#332c28"/>
+                <stop offset=".1651" stop-color="#332c28"/>
+                <stop offset=".5064" stop-color="#332c28"/>
+                <stop offset=".8061" stop-color="#332c28"/>
+              </linearGradient>
+              <linearGradient id="purple" gradientUnits="userSpaceOnUse" x1="58.0506" y1="80.3482" x2="58.0506" y2="36.8212" gradientTransform="matrix(1 0 0 -1 0 178.0107)">
+                <stop offset="0" stop-color="#332c28"/>
+                <stop offset=".1651" stop-color="#332c28"/>
+                <stop offset=".5064" stop-color="#45007f"/>
+                <stop offset=".8061" stop-color="#49006a"/>
+              </linearGradient>
+              <path class="hair" d="M41.5 128.1c-.5 3.3.1 17.8.6 18.4.7.7 13.1.9 15 .5 1.6-.3 15.6.3 16.7 1 1.5.8 2.8-28.6 2.2-30.9s-.3-24-.9-24.2c-3-1.5-34.5-2-34.8.3-1.6 11.8 1.7 31.7 1.2 34.9z" fill="url(#brown)"/>
+              <path class="hair" d="M85.6 124.1c-.4 3.3-.9 16.5-.5 17.1.5.7 9.9 2.4 11.2 2 1.1-.3 16.1-1.2 17-.5 1.1.8-2.3-27.3-2.7-29.6-.4-2.3 7.4-21.1 7-21.4-2.2-1.5-33.7-4.3-34-2-1.2 11.8 2.4 31.2 2 34.4z" fill="url(#brown)"/>
+              <path fill="#eee1d2" d="M63.6 105.8h29v41.3h-29z"/>
+              <path d="M92.6 124.9S82.5 122.3 79 122c-4.1-.4-15.4-2.7-15.4-2.7l.1-4.9 28.9-2v12.5z" opacity=".4" fill="#d5c9bd"/>
+              <path d="M122.7 60.6c0 16.1-3.7 30.8-14.1 40.8-9.1 8.7-18 15.7-31.2 15.7-13.4 0-21.4-4.7-30.5-13.7-10.1-10-16.5-22.5-16.5-38.3 0-10.2.2-18.2 1-29.3.6-9.1 7.4-14.1 16.5-19.9 8.5-5.4 20.4-6.3 29.7-6.3 12 0 26.9.8 35.1 8 5.7 5 9.8 9.2 9.8 16.5.1 10 .2 17.5.2 26.5z" fill="#eee1d2" stroke="#ecddcb" stroke-miterlimit="10"/>
+              <path class="st8" d="M36.1 82.3c-1.8-4.9-2.8-9.9-2.5-11.8 1.7-11.3.3-2.9 1.7-11.3.9-5.4 64.8-39.5 64.8-39.5L75.8.1s-48 1.1-57.9 24.3c-1.9 4.4-2.9 8.8-3.8 12.7-2.4 11.4-5.5 16.3-6 23.5-.6 8.5 4.3 16.2 5.1 24.8.2 1.7-3.3 11.5-1.1 11.6 4 .1 30.3 5.4 30.1.9-.2-5.3-6-15.4-6.1-15.6"/>
+              <path class="st8" d="M115.9 80.6c1.1-4.9 3.8-8.5 4-11.5.5-9-2-21.6-3-28.3-1.1-7.8-7.2-17.6-10-19-4.6-2.4-31.3-5.6-31.3-5.6V.1s49-2.3 57.6 21.5c7.9 21.8 7.6 25.6 8.2 34.8.6 8.5 2.7 7.8 2.3 19.6-.2 5.6-3.9 13.1-3.7 18.4.2 4.7-26.9 1-26.2.3"/>
+              <path class="hair" d="M11.8 113.2c.7 1.9 3.5 6.4 2.6 8.8-1.4 3.6-1.9 2.9-2.9 6.2-1 3.3.6 17.8 1.7 18.3 1.5.7 7.2-2.5 13-3.5 8.2-1.4 18 5.2 22 4.8 3.4-.3 2.8-11.5 3.9-14.1 1.2-2.8.1-6.6-1.1-8.9s-5.3-6.1-6.3-8.2c-2-4.2-1.3-5.2-2.4-17.8 0-.3-.9-4.9-1.7-6C37 87.3 12.7 89 12 91.3c-3.6 11.8-1.3 18.8-.2 21.9z" fill="url(#brown)"/>
+              <path class="hair" d="M103.4 120.1c-.5 2.5 1 5.1-.1 8.4-1 3.3.9 15.1 2.1 15.7.9.4 6.4-.8 8.8-.2 9 2.1 13.1 5.8 17.2 5.5 3.4-.3 3.3-4.8 5.5-6.8 2.4-2 .8-10.4-.3-12.6-1.2-2.5 5.2-12 4.8-16.1-.3-3.2-1.2-9.6-1.7-14.9 0-.3-.2-2-.1-4.2.3-6.6-25.3-2.8-26-.5-1.4 4.6-6 12.7-7.4 15.8-2.1 4.9-2.4 7.9-2.8 9.9z" fill="url(#brown)"/>
+              <path d="M120.2 176.8c-4 0-62.2-.2-72.8-.2-8.7 0-12.2-.6-10-6.5 4-11.1-5.3-28.7-.6-31.8 3.8-2.5 42.4-.6 48.1-.6 5.8 0 20.9.2 28.6 1.1 2 .2 3.9.1 4.6.2 7.1.6 6.1 37.8 2.1 37.8z" fill="#efe2d3"/>
+              <path class="st14" d="M15.3 176.8c-9.1.2-5.9 0-14.9 0-.6 0-.5 0 .5-1.9 1.2-2.4 3.5-7.4 4.8-11.2 4-11.1 7.7-23.9 13.2-24.9 3.2-.6 7.4-.9 12.3-1.2 5.1-.3 3.8 0 6.6 0 3.3 0 5.2 6.6 1.7 14.1-3.9 8.4-7 17.7-8.1 23.8-.1.4-.2 1.3-.5 1.3-3.5.2-7.7-.1-15.6 0z"/>
+              <path class="st14" d="M79.8 176.8c-20.4-.3-27.4 0-46.8 0-1.7 0-.2-4.5.1-6.2 1.3-8.4 4.8-33 4.8-33s26.7 14.5 39.8 14.5c15.5 0 40.1-13.2 40.1-13.2s5 22.5 6.4 30c.3 1.6 1.4 7.8-.3 7.8-10.9.1-24.4.4-44.1.1z"/>
+              <path class="st14" d="M140 176.8c9.1.1 6.3-.1 15.1 0 1.9 0-.8-5.3-3.1-11.6-4.1-11.1-9.2-23.4-14.7-24.5-2.2-.5-5.5-.5-10-1-4.5-.4-6.4-.6-8.7-.7-.5 0-1.5-.3-1.4.3.3 5.6-2.9 11.1 0 17.7 4.1 9.2 9.1 13.3 10.6 19.2.1.4.1.7.4.7 1.6-.1 5.1-.1 11.8-.1z"/>
+              <path fill="none" stroke="#4f4f4f" stroke-width=".25" stroke-miterlimit="10" d="M62.9 137.1l14.6 6.3 15.1-5.6"/>
+              <path fill="#4f4f4f" d="M78.6 142.7l-1.1 1.9-1.1-1.9z"/>
+            </svg>
+        </figure>
+      </section>
+      <section class="section__heading">
+        <h1 v-if="portuguese" class="h1">{{ hero.h1.pt }}</h1>
+        <h1 v-else class="h1">{{ hero.h1.en }}</h1>
+        <h2 v-if="portuguese" class="h2">{{ hero.h2.en }}</h2>
+        <h2 v-else class="h2">{{ hero.h2.en }}</h2>
+      </section>
     </section>
     <section class="section"> 
       <section-title name="About"></section-title>
@@ -57,22 +65,30 @@
 </template>
 
 <script>
-
+import sections from './../data/sections.json'
 export default {
   name: 'giuliacardieri',
+  data () {
+    return {
+      hero: sections.sections.hero,
+      portuguese: false
+    }
+  }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Solway&display=swap');
+
 body {
   margin: 0;
   padding: 0;
 }
 
 .container {
-  background-color: var(--color-primary);
-  color: var(--black);
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  background-color: var(--color-light);
+  color: var(--color-darker);
+  font-family: 'Solway', Helvetica, Arial, sans-serif;
   margin: 0;
   padding: 0;
   text-align: center;
@@ -81,8 +97,11 @@ body {
 }
 
 :root {
-  --color-primary: #746977;
-  --color-darker: #423745;
+  --color-main-1: #9C2855;
+  --color-main-2: #E14282;
+  --color4: #FF9E01;
+  --color-light: #FADCE4;
+  --color-darker: #7F2044;
   --white: #eee;
   --black: #282828;
   --grey: #555;
@@ -107,9 +126,15 @@ body {
 
 /* section */
 .section {
+  align-items: center;
+  animation: colorfy 3s ease-in-out 1;
+  background-color: var(--bgColor);
   box-sizing: border-box;
   display: flex;
+  flex-direction: column;
   height: 100vh;
+  justify-content: center;
+  width: 100vw;
 }
 
 .section--reversed {
@@ -133,16 +158,55 @@ body {
   width: 50vw;
 }
 
+.section__heading {
+  text-align: center;
+}
+
 .section__svg {
-  width: 30%;
+  margin-bottom: -1vw;
+  width: 80%;
+}
+
+.section__image {
+  align-items: flex-end;
+  background-color: var(--color-main-1);
+  border-radius: 100%;
+  display: flex;
+  height: 25vw;
+  justify-content: center;
+  min-height: 300px;
+  min-width: 300px;
+  overflow: hidden;
+  position: relative;
+  width: 25vw;
 }
 
 /* typography */
+
+.h1 {
+  font-size: 2rem;
+  margin: 1rem 0;
+}
+
 .h2 {
-  color: var(--white);
-  font-size: 2.5rem;
+  color: var(--color-main-2);
+  font-size: 1.5rem;
+  margin: 0.75rem 0;
 }
 
 /* svg drawing */
 .st4{display:none}.st4,.st8{fill:#332c28}.st14{fill:#1b1b1c}
+
+/* animation */
+@keyframes colorfy {
+  0% {
+    filter: grayscale(100%);
+  }
+  25% {
+    filter: grayscale(100%);
+  }
+  100% {
+    filter: grayscale(0);
+  }
+}
 </style>
