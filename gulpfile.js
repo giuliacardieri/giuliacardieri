c36f0c6bce84467db34f458db035e8ba77728e1f
Giulia Cardieri <giulia.cardieri@gmail.com>
2021-10-19 19:03:52 -0300

    Update icons folder on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('images', gulp.series(function() {
 }))
 
 gulp.task('copy-icons', function() {
-  return gulp.src(['images/**/*'])
+  return gulp.src(['images/icons/*'])
   	.pipe(gulp.dest('dist/images/icons'));
 })
 
